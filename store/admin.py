44c33267ff0d14404271ece6a44da78f4f48b2cc
vmitsek-amplify <vmitsek@amplify.com>
2022-01-30 21:27:39 +0200

    Create product model

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('product_name',)}
+    list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
+
+
+admin.site.register(Product, ProductAdmin)
