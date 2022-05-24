472889fc794adafd0790de3eceb7315b77af58e3
vmitsek-amplify <vmitsek@amplify.com>
2022-05-24 10:26:17 +0300

    Add context processor to display categories in dropdown menu

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,5 @@ from store import views
 
 urlpatterns = [
     path('', views.store, name='store'),
-    path('<slug:category_slug>/', views.store, name='category_by_slug'),
+    path('<slug:category_slug>/', views.store, name='products_by_category'),
 ]
