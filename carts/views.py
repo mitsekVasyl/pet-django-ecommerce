95ccc72c5bb8fb7f76edeb629b7d482992a331fd
vmitsek-amplify <vmitsek@amplify.com>
2022-05-26 21:11:48 +0300

    Implement decreasing and removing goods from cart

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from carts.models import Cart, CartItem
 from store.models import Product
@@ -34,6 +34,28 @@ def add_cart(request, product_id):
             cart=cart_obj
         )
         cart_item.save()
+
+    return redirect('cart')
+
+
+def remove_cart(request, product_id):
+    cart_obj = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart_obj)
+
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+
+def remove_cart_item(request, product_id):
+    cart_obj = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart_obj)
+    cart_item.delete()
 
     return redirect('cart')
 
