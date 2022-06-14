0741bfb0829cee01a83cbbab93812164468e3ebf
vmitsek-amplify <vmitsek@amplify.com>
2022-06-14 21:36:48 +0300

    Fix order model

--- orders/models.py
+++ orders/models.py
@@ -32,7 +32,7 @@ class Order(models.Model):
     phone_number = models.CharField(max_length=15)
     email = models.EmailField(max_length=50)
     address_line_1 = models.CharField(max_length=50)
-    address_line_2 = models.CharField(max_length=50)
+    address_line_2 = models.CharField(max_length=50, blank=True)
     country = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
@@ -46,7 +46,7 @@ class Order(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.user.first_name
+        return self.first_name
 
 
 class OrderProduct(models.Model):
