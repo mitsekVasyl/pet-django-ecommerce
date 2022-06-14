3c96bd291d91ccda1043a9fb4d59bf28d0903d84
vmitsek-amplify <vmitsek@amplify.com>
2022-06-14 22:05:39 +0300

    Orders preview page before payment

--- orders/views.py
+++ orders/views.py
@@ -7,6 +7,10 @@ from orders.forms import OrderForm
 from orders.models import Order
 
 
+def payments(request):
+    return render(request, 'orders/payments.html')
+
+
 def place_order(request, total=0, quantity=0):
     current_user = request.user
 
@@ -52,7 +56,16 @@ def place_order(request, total=0, quantity=0):
             order_number = current_date + str(order_data.id)
             order_data.order_number = order_number
             order_data.save()
-            return redirect('checkout')
+
+            order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
+            context = {
+                'order': order,
+                'cart_items': cart_items,
+                'total': total,
+                'tax': tax,
+                'grand_total': grand_total
+            }
+            return render(request, 'orders/payments.html', context)
 
     else:
         return redirect('checkout')
