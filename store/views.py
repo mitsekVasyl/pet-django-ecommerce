74432f5322689228152de4079583efbb90fc3d04
vmitsek-amplify <vmitsek@amplify.com>
2022-06-18 11:58:13 +0300

    Add view for submitting review. Template updates

--- store/views.py
+++ store/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 
 from category.models import Category
-from store.models import Product
+from store.forms import ReviewForm
+from store.models import Product, ReviewRating
 
 PRODUCTS_COUNT_ON_STORE_PAGE = 3
 
@@ -48,3 +51,32 @@ def search(request):
         'products_count': products_count,
     }
     return render(request, 'store/store.html', context)
+
+
+@login_required
+def submit_review(request, product_id):
+    url = request.META.get('HTTP_REFERER')
+    if request.method == "POST":
+        try:
+            reviews = ReviewRating.objects.get(user__id=request.user.id, product__id=product_id)
+            form = ReviewForm(request.POST, instance=reviews)
+            form.save()
+            messages.success(request, 'Thank you! Your review has been updated')
+
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            print(form)
+            print(form.cleaned_data['rating'])
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.rating = form.cleaned_data['rating']
+                data.review = form.cleaned_data['review']
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.product_id = product_id
+                data.user_id = request.user.id
+                data.save()
+
+                messages.success(request, 'Thank you! Your review has been submitted')
+
+        return redirect(url)
