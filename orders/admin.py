9461d44875c06731dcec3e209461b62298a84a10
vmitsek-amplify <vmitsek@amplify.com>
2022-06-18 08:07:22 +0300

    Store cart items in OrderProducts. Model and modelAdmin fixes

--- orders/admin.py
+++ orders/admin.py
@@ -3,11 +3,18 @@ from django.contrib import admin
 from orders.models import Payment, Order, OrderProduct
 
 
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+    readonly_fields = ('payment', 'user', 'product', 'quantity', 'product_price', 'ordered')
+    extra = 0
+
+
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['order_number', 'full_name', 'phone_number', 'email', 'city', 'order_total', 'tax', 'status', 'is_ordered']
     list_filter = ['status', 'is_ordered']
     search_fields = ['order_number', 'first_name', 'last_name', 'phone_number', 'email']
     list_per_page = 20
+    inlines = [OrderProductInline]
 
 
 admin.site.register(Payment)
--- orders/views.py
+++ orders/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect
 
 from carts.models import CartItem
 from orders.forms import OrderForm
-from orders.models import Order, Payment
+from orders.models import Order, Payment, OrderProduct
 
 
 def payments(request):
@@ -26,6 +26,24 @@ def payments(request):
     order.payment = payment
     order.is_ordered = True
     order.save()
+
+    cart_items = CartItem.objects.filter(user=current_user)
+    for item in cart_items:
+        orderproduct = OrderProduct()
+        orderproduct.order_id = order.id
+        orderproduct.payment = payment
+        orderproduct.user_id = current_user.id
+        orderproduct.product_id = item.product_id
+        orderproduct.quantity = item.quantity
+        orderproduct.product_price = item.product.price
+        orderproduct.ordered = True
+        orderproduct.save()
+
+
+    # reduce quantity of products
+    # clear a cart
+    # send email to customer
+
     return render(request, 'orders/payments.html')
 
 
