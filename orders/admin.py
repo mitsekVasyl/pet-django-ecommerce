0c6017ca33b8c42156c7eaac59b1c1114615c97e
vmitsek-amplify <vmitsek@amplify.com>
2022-06-18 07:51:09 +0300

    Customize order admin appearence

--- orders/admin.py
+++ orders/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 
 from orders.models import Payment, Order, OrderProduct
 
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_number', 'full_name', 'phone', 'email', 'city', 'order_tatal', 'tax', 'status', 'is_ordered']
+    list_filter = ['status', 'is_ordered']
+    search_fields = ['order_number', 'first_name', 'last_name', 'phone', 'email']
+    list_per_page = 20
+
+
 admin.site.register(Payment)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderProduct)
