a26d6265b2aab00ef2ed2d878af2bb527f2e207c
vmitsek-amplify <vmitsek@amplify.com>
2022-06-18 09:22:02 +0300

    Add model for review system

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from store.models import Product, Variation
+from store.models import Product, Variation, ReviewRating
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -16,3 +16,4 @@ class VariationAdmin(admin.ModelAdmin):
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation, VariationAdmin)
+admin.site.register(ReviewRating)
